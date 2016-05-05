e9036df6cfc231b020252f8473caa90e05c91ac6
xudafeng <xudafeng@126.com>
2016-05-05 23:24:09 +0800

    typo

--- test/utils.js
+++ test/utils.js
@@ -1,12 +1,25 @@
+/* ================================================================
+ * autoresponsive_react_native_sample by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Mon Jun 02 2014 20:15:51 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright 2015 xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
 'use strict';
 
-var BlinkDiff = require('blink-diff');
+const BlinkDiff = require('blink-diff');
 
 function diffImage(imageAPath, imageB, threshold, outputPath) {
   return new Promise((resolve, reject) => {
     var diff = new BlinkDiff({
       imageAPath: imageAPath, // Path
-      imageB: imageB, // Buffer
+      imageB: imageB,         // Buffer
       thresholdType: BlinkDiff.THRESHOLD_PERCENT,
       threshold: threshold,
       imageOutputPath: outputPath
@@ -18,7 +31,7 @@ function diffImage(imageAPath, imageB, threshold, outputPath) {
       }
       var ifPassed = diff.hasPassed(result.code);
       console.log(ifPassed ? 'Image Comparison Passed' : 'Image Comparison Failed');
-      console.log('Found ' + result.differences + ' pixel differences between two images.');
+      console.log(`Found ${result.differences} pixel differences between two images.`);
       resolve(ifPassed);
     });
   });
