35ca44c2ba7f542d13d19a65b3fef0be162b42a5
zenzhu <zic.zhu@gmail.com>
2016-05-05 20:57:11 +0800

    Add screenshot diff

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ $ react-native run-android
 
 iOS
 
-![iOS](./screenshot/ios.gif)
+![iOS](./screenshot/ios.png)
 
 Android
 
---
+++ test/utils.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var BlinkDiff = require('blink-diff');
+
+function diffImage(imageAPath, imageB, threshold, outputPath) {
+  return new Promise((resolve, reject) => {
+    var diff = new BlinkDiff({
+      imageAPath: imageAPath, // Path
+      imageB: imageB, // Buffer
+      thresholdType: BlinkDiff.THRESHOLD_PERCENT,
+      threshold: threshold,
+      imageOutputPath: outputPath
+    });
+
+    diff.run(function(err, result) {
+      if (err) {
+        return reject(err);
+      }
+      var ifPassed = diff.hasPassed(result.code);
+      console.log(ifPassed ? 'Image Comparison Passed' : 'Image Comparison Failed');
+      console.log('Found ' + result.differences + ' pixel differences between two images.');
+      resolve(ifPassed);
+    });
+  });
+}
+
+module.exports = {
+  diffImage
+};
